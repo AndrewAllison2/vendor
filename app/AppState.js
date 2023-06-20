965ece5a9d1f1a728b075a8fc3edcbd737df60ce
Andrew <andrewallison5211@gmail.com>
2023-06-20 16:57:28 -0600

    Its a vendr

--- app/AppState.js
+++ app/AppState.js
@@ -21,6 +21,9 @@ class ObservableAppState extends EventEmitter {
     new Snack({ name: 'Cake', price: 5, imgUrl: 'https://images.unsplash.com/photo-1621303837174-89787a7d4729?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8OHx8Y2FrZXxlbnwwfHwwfHx8MA%3D%3D&auto=format&fit=crop&w=500&q=60' }),
     new Snack({ name: 'Pop Tarts', price: 1.75, imgUrl: 'https://plus.unsplash.com/premium_photo-1663853560438-6cc3a70a3c97?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8cG9wJTIwdGFydHN8ZW58MHx8MHx8fDA%3D&auto=format&fit=crop&w=500&q=60' }),
     new Snack({ name: 'Meatballs', price: 7.75, imgUrl: 'https://images.unsplash.com/photo-1529042410759-befb1204b468?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8bWVhdGJhbGx8ZW58MHx8MHx8fDA%3D&auto=format&fit=crop&w=500&q=60' }),
+    new Snack({ name: 'Burrito', price: 4, imgUrl: 'https://media.istockphoto.com/id/1361612471/photo/a-burrito-with-dips-cut-in-half-on-a-wooden-board.webp?b=1&s=170667a&w=0&k=20&c=wgGIr5Yu9yu8XffLfI440O3bntldqPlY8sVWWGEIlBM=' }),
+    new Snack({ name: 'Lobster', price: 9, imgUrl: 'https://media.istockphoto.com/id/117149629/photo/close-up-of-fresh-steamed-lobster-with-herbs-in-grey-plate.webp?b=1&s=170667a&w=0&k=20&c=xVbkX_bjAhvRKcUNKMjzrm5fTGyew28xWZ5IiO4nZk8=' }),
+    new Snack({ name: 'Understanding MVC', price: 100, imgUrl: 'https://media.istockphoto.com/id/1455345645/photo/bearded-hispanic-man-in-red-sweater-frowning-and-looking-dissatisfied-while-scratching-his.webp?b=1&s=170667a&w=0&k=20&c=MeGD5RELr9nx78aQCB5cFAonkmpVYVEY5baQObpucWE=' }),
   ]
 
   // NOTE Used to load initial data
--- app/models/Snacks.js
+++ app/models/Snacks.js
@@ -8,16 +8,25 @@ export class Snack {
 
   get snackTemplate() {
     return /*html*/ `
-    <div class="col-4 m-auto">
-        <div class="bg-secondary text-center">
-          <img class="chips-img py-3"
-            src="${this.imgUrl}"
-            alt="">
-          <p>${this.name}</p>
-          <p>Price:$${this.price}</p>
-          <button class="btn btn-success p-1 mb-2" onclick="app.SnacksController.buySnack('${this.name}')">Buy ${this.name}</button>
-        </div>
-      </div>`
+    <div class="col-4 p-2">
+  <img src="${this.imgUrl}" class="chips-img card-img-top" alt="...">
+  <div class="card-body">
+    <h5 class="card-title">${this.name}</h5>
+    <p class="card-text">${this.price}</p>
+    <button class="btn btn-success p-1 mb-2" onclick="app.SnacksController.buySnack('${this.name}')">Buy ${this.name}</button>
+  </div>
+</div>`
+
+
+    // <div class="col-4 m-auto">
+    //     <div class="bg-secondary text-center">
+    //       <img class="chips-img py-3"
+    //         src="${this.imgUrl}"
+    //         alt="">
+    //       <p>${this.name}</p>
+    //       <p>Price:$${this.price}</p>
+    //     </div>
+    //   </div>`
   }
 
 }
